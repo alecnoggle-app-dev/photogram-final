9b26fdd3569068807ab4b53e605af464c6da7a09
alecnoggle <anoggle@chicagobooth.edu>
2022-03-14 03:03:26 +0000

    Quick changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -31,9 +31,9 @@ class PhotosController < ApplicationController
 
     if the_photo.valid?
       the_photo.save
-      redirect_to("/photos", { :notice => "Photo created successfully." })
+      redirect_to("/", { :notice => "Photo created successfully." })
     else
-      redirect_to("/photos", { :notice => "Photo failed to create successfully." })
+      redirect_to("/", { :notice => "Photo failed to create successfully." })
     end
   end
 
